cceadc32651cd815b4e00dd7ec26600015d8be8c
lkhoa103 <lkhoa103@gmail.com>
2019-03-01 06:59:05 -0500

    Create title entity

---
+++ src/main/java/com/khoa/mentorship/entity/Title.java
@@ -0,0 +1,38 @@
+package com.khoa.mentorship.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TITLE")
+public class Title {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "title_id")
+	private int titleId;
+	
+	@Column(name="title")
+	private String title;
+
+	public int getTitleId() {
+		return titleId;
+	}
+
+	public void setTitleId(int titleId) {
+		this.titleId = titleId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+}
