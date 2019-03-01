b975208d5eb19c15907d1cf9825ce72b4047d49f
lkhoa103 <lkhoa103@gmail.com>
2019-03-01 06:44:02 -0500

    Create university entity

---
+++ src/main/java/com/khoa/mentorship/entity/University.java
@@ -0,0 +1,49 @@
+package com.khoa.mentorship.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="UNIVERSITY")
+public class University {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "university_id")
+	private int universityId;
+	
+	@Column(name = "university_name")
+	private String universityName;
+	
+	@Column(name = "university_address")
+	private String universityAddress;
+
+	public int getUniversityId() {
+		return universityId;
+	}
+
+	public void setUniversityId(int universityId) {
+		this.universityId = universityId;
+	}
+
+	public String getUniversityName() {
+		return universityName;
+	}
+
+	public void setUniversityName(String universityName) {
+		this.universityName = universityName;
+	}
+
+	public String getUniversityAddress() {
+		return universityAddress;
+	}
+
+	public void setUniversityAddress(String universityAddress) {
+		this.universityAddress = universityAddress;
+	}
+
+}
