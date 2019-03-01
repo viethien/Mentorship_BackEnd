5634508f0ac13a8658eb02702ae9b5c1b5198471
lkhoa103 <lkhoa103@gmail.com>
2019-03-01 07:06:00 -0500

    Create student entity

---
+++ src/main/java/com/khoa/mentorship/entity/Student.java
@@ -0,0 +1,31 @@
+package com.khoa.mentorship.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="STUDENT")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "student_id")
+	private int studentId;
+	
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+
+}
