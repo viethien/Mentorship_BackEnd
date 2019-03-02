5d842b4f09171901335c4acaa5e908d1fca16ce2
lkhoa103 <lkhoa103@gmail.com>
2019-03-01 20:11:21 -0500

    Add registerDAO

---
+++ src/main/java/com/khoa/mentorship/dao/RegisterDAO.java
@@ -0,0 +1,9 @@
+package com.khoa.mentorship.dao;
+
+import com.khoa.mentorship.model.RegisterModel;
+
+public interface RegisterDAO {
+	
+	public boolean registerUser(RegisterModel registerModel);
+
+}
