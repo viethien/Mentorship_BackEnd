705ffaaf58ce62be24b15d2e4f813c5af173ae10
lkhoa103 <lkhoa103@gmail.com>
2019-03-01 20:20:57 -0500

    Implement RegisterDAO and add RegisterService

---
+++ src/main/java/com/khoa/mentorship/dao/impl/RegisterDAOImpl.java
@@ -0,0 +1,42 @@
+package com.khoa.mentorship.dao.impl;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.khoa.mentorship.dao.RegisterDAO;
+import com.khoa.mentorship.entity.Mentor;
+import com.khoa.mentorship.entity.Student;
+import com.khoa.mentorship.model.RegisterModel;
+import com.khoa.mentorship.model.Types;
+
+public class RegisterDAOImpl implements RegisterDAO {
+
+	@Autowired
+	private EntityManager entityManager;
+	
+	@Override
+	public boolean registerUser(RegisterModel registerModel) {
+		Session currentSession = entityManager.unwrap(Session.class);
+		if(registerModel.getType() == Types.HIGHSCHOOL_STUDENT) {
+			Student student = new Student();
+			student.setStudentId(0);
+			student.setFirstName(registerModel.getFirstName());
+			student.setLastName(registerModel.getLastName());
+			student.setEmail(registerModel.email);
+			student.setPassword(registerModel.getPassword());
+			currentSession.saveOrUpdate(student);
+		} else if (registerModel.getType() == Types.PROFESSOR) {
+			Mentor mentor = new Mentor();
+			mentor.setMentorId(0);
+			mentor.setFirstName(registerModel.getFirstName());
+			mentor.setLastName(registerModel.getLastName());
+			mentor.setEmail(registerModel.getEmail());
+			mentor.setPassword(registerModel.getPassword());
+			currentSession.saveOrUpdate(mentor);
+		}
+		return true;
+	}
+
+}
---
+++ src/main/java/com/khoa/mentorship/service/RegisterService.java
@@ -0,0 +1,9 @@
+package com.khoa.mentorship.service;
+
+import com.khoa.mentorship.model.RegisterModel;
+
+public interface RegisterService {
+	
+	public boolean registerUser(RegisterModel registerModel);
+
+}
