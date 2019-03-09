e3f1573399bb86495f9a87c28733c77e5a5dd12b
lkhoa103 <lkhoa103@gmail.com>
2019-03-09 07:02:56 -0500

    Add LoginReponseModel

---
+++ src/main/java/com/khoa/mentorship/model/LoginResponseModel.java
@@ -0,0 +1,25 @@
+package com.khoa.mentorship.model;
+
+public class LoginResponseModel {
+	
+	public boolean isSuccessful;
+	
+	public String userType;
+
+	public boolean isSuccessful() {
+		return isSuccessful;
+	}
+
+	public void setSuccessful(boolean isSuccessful) {
+		this.isSuccessful = isSuccessful;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+}
